7941eda896e63b27ed584a8edc18ab8a7d9854f2
luca9294 <luca9294@hotmail.it>
2017-01-20 21:39:49 +0100

    Bluetooth devices are now detected and listed

--- app/src/main/java/arduino/controller/MainActivity.java
+++ app/src/main/java/arduino/controller/MainActivity.java
@@ -1,14 +1,143 @@
 package arduino.controller;
 
 import android.app.Activity;
+import android.app.DialogFragment;
+import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "BlueTest5-MainActivity";
+    private int mMaxChars = 50000;//Default
+    private UUID mDeviceUUID;
+    private BluetoothSocket mBTSocket;
+    private boolean mIsUserInitiatedDisconnect = false;
+    private boolean mIsBluetoothConnected = false;
+    private BluetoothAdapter mBTAdapter;
+    private List<BluetoothDevice> ls;
+    private List<String> ls_string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter();
+        ls = new ArrayList<BluetoothDevice>();
+        ls_string = new ArrayList<String>();
+        Button btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isBluehtoothActive()){
+                    showMsgBox("Error", "Bluetooth not enabled");
+                }
+                else
+                {
+                    try {
+                        new SearchDevices().execute().get();
+                        if (ls.size() == 0)
+                            showMsgBox("Error", "Not device paired");
+                        else
+                            openCheckBoxesDialog();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                }
+            }
+        }
+    });
+    }
+
+
+    //Check whether the bluetooht is active or not
+    private boolean isBluehtoothActive(){
+            if (!mBTAdapter.isEnabled())
+                return false;
+            else
+                return true;
+        }
+
+    private void showMsgBox(String title, String text){
+        new AlertDialog.Builder(this)
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+    private void openCheckBoxesDialog()
+    {
+        int checked = 0;
+        final CharSequence[] dialogList=  ls_string.toArray(new CharSequence[ls_string.size()]);
+        final AlertDialog.Builder builderDialog = new AlertDialog.Builder(MainActivity.this);
+        builderDialog.setTitle("Paired Devices");
+        builderDialog.setSingleChoiceItems(dialogList, checked, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builderDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builderDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alert = builderDialog.create();
+        alert.show();
+    }
+
+
+    private class SearchDevices extends AsyncTask<Void, Void, List<BluetoothDevice>> {
+
+        @Override
+        protected List<BluetoothDevice> doInBackground(Void... params) {
+            Set<BluetoothDevice> pairedDevices = mBTAdapter.getBondedDevices();
+            List<BluetoothDevice> listDevices = new ArrayList<BluetoothDevice>();
+            ls.clear();
+            ls_string.clear();
+            for (BluetoothDevice device : pairedDevices) {
+                ls.add(device);
+                ls_string.add(device.getName());
+            }
+            return ls;
+        }
     }
 }
