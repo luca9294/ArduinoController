87187fa2ccdaf2d992439dd94d8bf5182336519c
luca9294 <luca9294@hotmail.it>
2017-01-22 10:58:50 +0100

    Settings

--- app/src/main/java/arduino/controller/MainActivity.java
+++ app/src/main/java/arduino/controller/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.DialogFragment;
 import android.bluetooth.BluetoothAdapter;
 import android.content.ComponentName;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
@@ -14,6 +15,8 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -67,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
     });
     }
 
-
     //Check whether the bluetooht is active or not
     private boolean isBluehtoothActive(){
             if (!mBTAdapter.isEnabled())
@@ -93,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
     }
-
 
     private void openCheckBoxesDialog()
     {
@@ -122,6 +123,30 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog alert = builderDialog.create();
         alert.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.action, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 
---
+++ app/src/main/java/arduino/javaclasses/ConnectionManager.java
@@ -0,0 +1,17 @@
+package arduino.javaclasses;
+
+/**
+ * Created by lucapellegrini1 on 22.01.17.
+ */
+
+public class ConnectionManager {
+
+
+
+
+
+
+
+
+
+}
